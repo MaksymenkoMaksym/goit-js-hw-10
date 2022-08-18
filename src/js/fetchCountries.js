3686a039e644e8ca444e64a900d1d10a1ed70798
Maksymenko Maksym <maksymenko1992@ukr.net>
2022-08-19 02:20:23 +0300

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,5 +1,5 @@
 export function fetchCountries(name = '') {
-    return fetch('https://restcountries.com/v2/al?fields=name,capital,languages,flags,population')
+    return fetch('https://restcountries.com/v2/all?fields=name,capital,languages,flags,population')
         .then(
             (response) => {
                 console.log(response);
